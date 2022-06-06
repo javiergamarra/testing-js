12dc1e3937ed71b0b26c404ef53b623e3bd9e800
Javier <nhpatt@gmail.com>
2022-06-07 01:38:00 +0200

    2 - implementar y testear piedra, papel, tijeras, lagarto y spock

--- 2 - spock/spock.js
+++ 2 - spock/spock.js
@@ -1,4 +1,13 @@
-function spock(){
-   return "FIXME!"
+const rules = {
+  spock: ['tijeras', 'piedra'],
+  papel: ['piedra', 'spock'],
+  piedra: ['tijeras', 'lagarto'],
+  tijeras: ['papel', 'lagarto'],
+  lagarto: ['papel', 'spock'],
+};
+
+function spock(primero, segundo) {
+  return primero === segundo ? 'empate' : rules[primero].includes(segundo) ? primero : segundo;
 }
+
 module.exports = spock;
--- 2 - spock/spock.test.js
+++ 2 - spock/spock.test.js
@@ -1,5 +1,33 @@
 const spock = require('./spock');
 
 test('papel gana a piedra', () => {
-    expect(spock('papel', 'piedra')).toBe('papel');
+  expect(spock('papel', 'piedra')).toBe('papel');
+});
+
+test('papel contra papel devuelve empate', () => {
+  expect(spock('papel', 'papel')).toBe('empate');
+});
+
+test('papel contra tijeras pierde', () => {
+  expect(spock('papel', 'tijeras')).toBe('tijeras');
+});
+
+test('piedra contra tijeras gana', () => {
+  expect(spock('piedra', 'tijeras')).toBe('piedra');
+});
+
+test('spock contra tijeras gana', () => {
+  expect(spock('spock', 'tijeras')).toBe('spock');
+});
+
+test('spock contra piedra gana', () => {
+  expect(spock('spock', 'piedra')).toBe('spock');
+});
+
+test('papel contra lagarto pierde', () => {
+  expect(spock('papel', 'lagarto')).toBe('lagarto');
+});
+
+test('spock contra lagarto pierde', () => {
+  expect(spock('spock', 'lagarto')).toEqual('lagarto');
 });
