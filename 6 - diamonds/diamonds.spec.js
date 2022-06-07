70e3a50930d262edc25f2eefada37d8300fe56a6
Javier <nhpatt@gmail.com>
2022-06-07 08:41:35 +0200

    6 - minando diamantes de forma asíncrona

--- 6 - diamonds/diamonds.spec.js
+++ 6 - diamonds/diamonds.spec.js
@@ -1,4 +1,18 @@
-async function mine(s) {}
+async function mine(s) {
+  let diamonds = 0;
+
+  while (s.includes('<>')) {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        diamonds = +1;
+        s = s.replace('<>', '');
+        resolve();
+      }, 100);
+    });
+  }
+
+  return diamonds;
+}
 
 test('we can mine diamonds', async () => {
   await expect(mine('')).resolves.toBe(0);
