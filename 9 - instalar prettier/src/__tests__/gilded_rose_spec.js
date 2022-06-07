f2594b63c0895342f8f87141a6513e6c0724ea50
Javier <nhpatt@gmail.com>
2022-06-07 07:41:12 +0200

    9 - instalar prettier

---
+++ 9 - instalar prettier/src/__tests__/gilded_rose_spec.js
@@ -0,0 +1,9 @@
+import { Shop, Item } from "../gilded_rose";
+
+describe("Gilded Rose", () => {
+  it("should foo", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("fixme");
+  });
+});
