62b7a04cd581c0741867270a2d41a1a5973c3cb9
Javier <nhpatt@gmail.com>
2022-06-07 08:41:48 +0200

    7 - nuestro propio jest

--- 7 - nuestro jest/jest.js
+++ 7 - nuestro jest/jest.js
@@ -1,3 +1,21 @@
+const expect = (value) => ({
+  toBe(expected) {
+    if (expected !== value) {
+      throw new Error(`expected ${expected} is not the same as ${value}`);
+    }
+  },
+});
+
+const test = (title, fun) => {
+  try {
+    fun();
+    console.log('fine!');
+  } catch (e) {
+    console.log(e);
+    console.log('error!');
+  }
+};
+
 const sum = (a, b) => a + b;
 
 test('sum suma', () => {
