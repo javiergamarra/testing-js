ee957726eff10dc91f3198df299ddf5d64185b1b
Javier <nhpatt@gmail.com>
2022-06-08 19:31:27 +0200

    16 - solución al game of life

--- 16 - Game of Life/game-of-life.test.js
+++ 16 - Game of Life/game-of-life.test.js
@@ -1,3 +1,59 @@
+class Board {
+  board = [];
+
+  length;
+
+  constructor(length) {
+    this.length = length;
+    this.board = Array.from({ length }).map(() => Array.from({ length }).map(() => false));
+  }
+
+  static newFromArray(board) {
+    const board1 = new Board();
+    board1.board = board;
+    board1.length = board.length;
+    return board1;
+  }
+
+  validPos(x) {
+    return x >= 0 && x < this.length;
+  }
+
+  getNeighbours(i, j) {
+    const pos = [-1, 0, 1];
+    let alive = 0;
+
+    for (let k = 0; k < pos.length; k += 1) {
+      for (let l = 0; l < pos.length; l += 1) {
+        if (this.validPos(i + pos[k]) && this.validPos(j + pos[l]) && !(pos[k] === 0 && pos[l] === 0)) {
+          if (this.board[i + pos[k]][j + pos[l]]) {
+            alive += 1;
+          }
+        }
+      }
+    }
+
+    return alive;
+  }
+
+  tick() {
+    const newBoard = new Board(this.length);
+    for (let i = 0; i < this.length; i += 1) {
+      for (let j = 0; j < this.length; j += 1) {
+        const n = this.getNeighbours(i, j);
+        if (n < 2 || n > 3) {
+          newBoard.board[i][j] = false;
+        } else if (n === 3) {
+          newBoard.board[i][j] = true;
+        } else {
+          newBoard.board[i][j] = this.board[i][j];
+        }
+      }
+    }
+    this.board = newBoard.board;
+  }
+}
+
 describe("board", () => {
   test("new board", () => {
     const board = new Board(3);
