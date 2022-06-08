51c77b50032a6c74d6b5d414357e28d2b8538f13
Javier <nhpatt@gmail.com>
2022-06-08 19:30:59 +0200

    15 - solución a snapshot testing

--- 15 - snapshot testing/snapshots.test.js
+++ 15 - snapshot testing/snapshots.test.js
@@ -1,9 +1,13 @@
-describe('matchers', () => {
-  it('snapshot testing', () => {
-    const user = {
-      name: 'LeBron James1',
-    };
+describe("snapshots capturan el valor inicial", () => {
+  const something = { key: "valu" };
 
-    expect(user).toMatchSnapshot();
+  it("guardamos el valor", () => {
+    expect(something).toMatchSnapshot();
+    something.nuevaKey = "nuevoVal2or";
+    expect(something).toMatchSnapshot();
   });
+
+  // it("mutamos el valor", () => {
+  //   expect(something).toMatchSnapshot();
+  // });
 });
